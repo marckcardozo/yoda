7ee334c947ef9ed830a8990ceb4dc9cef947a61e
Marcelo Cardozo <root@devops>
2018-06-19 22:32:50 +0000

    Script da Padme

--- padme.sh
+++ padme.sh
@@ -2,5 +2,6 @@
 
 for ip in 10 20 30 40;
 do
-	ping -c2 192.168.99.$IP >- && echo "Com Conexao" || echo "Sem Conexao"
+	ping -c2 192.168.99.$IP >- && echo "Com Conexao" || echo "Sem Conexao";
+done
 
